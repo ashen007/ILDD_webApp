f14576838656a862c1c8e19afe6a3c2f0bb0f2db
Hewarathna Ashen Iranga <48565752+ashen007@users.noreply.github.com>
2022-01-02 18:08:20 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import dash
-
-app = dash.Dash(__name__,
-                suppress_callback_exceptions=True,
-                external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'],
-                assets_folder='assets/')
-
-server = app.server
